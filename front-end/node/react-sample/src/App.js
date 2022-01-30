b8894bd612146fe970697dfc9a96ac034df00d1e
github Kazuki-oss <imukazu0227@gmail.com>
2022-01-30 13:00:24 +0900

    getiing current position  was done

--- front-end/node/react-sample/src/App.js
+++ front-end/node/react-sample/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, Marker, marker } from "@react-google-maps/api";
+import { useState } from "react";
 
 const containerStyle = {
   width: "400px",
@@ -12,15 +13,33 @@ const center = {
 };
 
 function App() {
+  const [position, setPosition] = useState({
+    lat: null,
+    lng: null,
+  });
+  const myPosition = {
+    lat: position.latitude,
+    lng: position.longitude,
+  };
+
+  const getCurrentPosition = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      setPosition({ latitude, longitude });
+    });
+  };
   return (
     <div className="App">
       <header className="App-header">
         <LoadScript googleMapsApiKey={process.env.GOOGLE_MAP_API_KEY}>
           <GoogleMap
             mapContainerStyle={containerStyle}
-            center={center}
+            center={myPosition}
             zoom={17}
-          ></GoogleMap>
+            onLoad={getCurrentPosition}
+          >
+            <Marker position={myPosition}></Marker>
+          </GoogleMap>
         </LoadScript>
       </header>
     </div>
