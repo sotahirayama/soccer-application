d5afda0f3cfa38a638ae3d6532279ce693e3569b
yoshinorihanada <yoshihana747@gmail.com>
2022-02-27 17:43:06 +0900

    displayed mock events information from own API server

--- back-end/cmd/hello/main.go
+++ back-end/cmd/hello/main.go
@@ -8,13 +8,18 @@ import (
 )
 
 type Event struct {
-    Title   string `json:"Title"`
-    Desc    string `json:"desc"`
+    Id int `json:"id"`
+    Name   string `json:"name"`
+    Place    string `json:"place"`
     // Date string `json:"date"`
 	Location string `json:"location"`
 }
 
 type Events []Event
+
+func enableCors(w *http.ResponseWriter) {
+    (*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -22,17 +27,19 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequests() {
+    
     http.HandleFunc("/", homePage)
     http.HandleFunc("/events", returnAllEvents)
     log.Fatal(http.ListenAndServe(":8081", nil))
 }
 func returnAllEvents(w http.ResponseWriter, r *http.Request) {
+    enableCors(&w)
     events := Events{}
     for i := 0; i < 10; i++ {
-        title := "Hello_%d"
+        t := "Hello_%d"
         events = append(
             events,
-            Event{Title: fmt.Sprintf(title, i), Desc: "Event Description", Location: "Event Location"})
+            Event{Id: i,Name: fmt.Sprintf(t, i), Place: "Showa kinen kouen", Location: "lat:35.69575,lng:139.77521"})
     }
     fmt.Println("Endpoint Hit: returnAllEvents")
     json.NewEncoder(w).Encode(events)
